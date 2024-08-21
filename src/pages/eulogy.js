20cf47b0de020b681664496d5241ee2478783199
Victorlangat <111707177+Victorlangat@users.noreply.github.com>
2024-08-21 16:59:12 +0300

    eulogy and memories page fixed

--- src/pages/eulogy.js
+++ src/pages/eulogy.js
@@ -1,30 +1,30 @@
 import React from "react";
+import './eulogy.css';
 import Navigation from "../components/navbar";
 import Footer from "../components/footer";
-
+import background from '../pics/background.jpg';
 
 function story() {
-    return(
-        <div className="body">
+  return (
+    <div className="body">
+      <div className="navbar">
+        <Navigation />
+      </div>
 
-            <div className="navbar">
-                <Navigation/>
-
-            </div>
-
-            <div className="the_story">
-
-
-            </div>
-            <div className="footer">
-                <Footer/>
-
-            </div>
-
-
+      <div className="the_story">
+        <p>
+          <span className="styled" >I</span>n the quiet stillness of this moment, we gather to honor the life of <span className="styled" >B</span>etty Langat. A gentle soul, a loving heart, and a spirit that touched the lives of all who knew her. Betty was a beacon of kindness, always ready to lend a helping hand or offer a comforting word. Their laughter was contagious, their smile could brighten any room, and their presence was a gift to those fortunate enough to share their company.
+        </p>
+        <div className="backgroundimage" >
+            <img src={background} alt="" />
         </div>
+      </div>
 
-    );
-    
-  }
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
 export default story;
